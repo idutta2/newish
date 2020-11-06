d40bdf995c03bfe63e509490ce64c5c984bf3732
Isha Dutta <ishadutta95@gmail.com>
2020-11-06 15:32:22 -0500

    Update trial.py

--- trial.py
+++ trial.py
@@ -1,2 +1,13 @@
 ## NEW FILE ADD HERE
 import temp
+import os 
+import sys
+
+class trial_class():
+	def __init__():
+		return
+
+	def trial_func1(self, id):
+		if id== "1":
+			return true
+		return false
